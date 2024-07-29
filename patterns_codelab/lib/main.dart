deca923a1a904462edaab29c85f1c848311e141a
lyy720301 <444109668@qq.com>
2024-07-29 16:08:09 +0800

    patterns_codelab: object pattern

--- patterns_codelab/lib/main.dart
+++ patterns_codelab/lib/main.dart
@@ -6,6 +6,24 @@ void main() {
   runApp(const DocumentApp());
 }
 
+/// switch表达式
+String formatDate(DateTime dateTime) {
+  final today = DateTime.now();
+  final difference = dateTime.difference(today);
+
+  return switch (difference) {
+    Duration(inDays: 0) => 'today',
+    Duration(inDays: 1) => 'tomorrow',
+    Duration(inDays: -1) => 'yesterday',
+    Duration(inDays: final days) when days > 7 =>
+      '${days ~/ 7} weeks from now', // Add from here
+    Duration(inDays: final days) when days < -7 =>
+      '${days.abs() ~/ 7} weeks ago', // to here.
+    Duration(inDays: final days, isNegative: true) => '${days.abs()} days ago',
+    Duration(inDays: final days) => '$days days from now',
+  };
+}
+
 class DocumentApp extends StatelessWidget {
   const DocumentApp({super.key});
 
@@ -32,23 +50,16 @@ class DocumentScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final (title, modified: lastmodified) = document.metadata; // Modify
     // final (title, :modified) = document.metadata; // Modify
+    final formattedModifiedDate = formatDate(lastmodified);            // Add this line
 
     final blocks = document.blocks;
-    var blockCenterList = [
-      for (final block in blocks)
-        Center(
-          child: Text(
-            'type: ${block.type}, text: ${block.text}', // And this line.
-          ),
-        ),
-    ];
     return Scaffold(
       appBar: AppBar(
         title: Text(title), // Modify this line,
       ),
       body: Column(
         children: [
-          Text('Last modified: $lastmodified'),                        // Modify from here
+          Text('Last modified: $formattedModifiedDate'), // Modify from here
           Expanded(
             child: ListView.builder(
               itemCount: blocks.length,
@@ -56,7 +67,7 @@ class DocumentScreen extends StatelessWidget {
                 return BlockWidget(block: blocks[index]);
               },
             ),
-          ),                                           
+          ),
         ],
       ),
     );
@@ -73,15 +84,22 @@ class BlockWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    TextStyle? textStyle;
-    switch (block.type) {
-      case 'h1':
-        textStyle = Theme.of(context).textTheme.displayMedium;
-      case 'p' || 'checkbox':
-        textStyle = Theme.of(context).textTheme.bodyMedium;
-      case _:
-        textStyle = Theme.of(context).textTheme.bodySmall;
-    }
+    // TextStyle? textStyle;
+    // switch (block.type) {
+    //   case 'h1':
+    //     textStyle = Theme.of(context).textTheme.displayMedium;
+    //   case 'p' || 'checkbox':
+    //     textStyle = Theme.of(context).textTheme.bodyMedium;
+    //   case _:
+    //     textStyle = Theme.of(context).textTheme.bodySmall;
+    // }
+    // switch expression
+    TextStyle? textStyle; // Modify from here
+    textStyle = switch (block.type) {
+      'h1' => Theme.of(context).textTheme.displayMedium,
+      'p' || 'checkbox' => Theme.of(context).textTheme.bodyMedium,
+      _ => Theme.of(context).textTheme.bodySmall
+    };
 
     return Container(
       margin: const EdgeInsets.all(8),
